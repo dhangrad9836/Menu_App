f7320ea8b2424e914f3f4c8b49f83d7781db677b
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-12-10 09:11:13 -0500

    Conveted to switch statement and added menu processing

--- Menu_App/Program.cs
+++ Menu_App/Program.cs
@@ -77,24 +77,32 @@ for(int i = 0; i < maxPets; i++)
     ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 }//end for loop
 
-//display the top-level menu options
+//do/while loop to process the menu
+do
+{
 
-Console.Clear();
+    //display the top-level menu options
 
-Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-Console.WriteLine(" 1. List all of our current pet information");
-Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-Console.WriteLine(" 5. Edit an animal’s age");
-Console.WriteLine(" 6. Edit an animal’s personality description");
-Console.WriteLine(" 7. Display all cats with a specified characteristic");
-Console.WriteLine(" 8. Display all dogs with a specified characteristic");
-Console.WriteLine();
-Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+    Console.Clear();
 
-//get user menu choice selection and store in readResult
-readResult = Console.ReadLine();
+    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+    Console.WriteLine(" 1. List all of our current pet information");
+    Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+    Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+    Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+    Console.WriteLine(" 5. Edit an animal’s age");
+    Console.WriteLine(" 6. Edit an animal’s personality description");
+    Console.WriteLine(" 7. Display all cats with a specified characteristic");
+    Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+    Console.WriteLine();
+    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+    //get user menu choice selection and store in readResult
+    readResult = Console.ReadLine();
+    //keep iterating until user enters "exit"
+} while (menuSelection != "exit");
+//end do/while loop
+
 if (readResult != null)
 {
     menuSelection = readResult.ToLower();
